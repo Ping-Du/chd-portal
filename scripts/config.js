d4dd0538d26e2d5afa97d20960568fb51069228b
Ping Du <pingpng32@gmail.com>
2016-10-20 20:02:18 +0800

    change config to production

--- scripts/config.js
+++ scripts/config.js
@@ -5,17 +5,20 @@ define([], function () {
         webRoot:'https://www.chdestination.com/', // production web US
         //webRoot:'https://chdtraceweb-hk.azurewebsites.net/', // production web HK
         //webRoot: 'http://localhost:63342/chd-portal/', // local host
+        //webRoot: 'https://chdtracewebdemo.azurewebsites.net/', // demo site
 
-        //apiRoot:'https://link.chdestination.com/', // production api US, not work
+        // -- apiRoot:'https://link.chdestination.com/', // production api US, not work
         apiRoot: 'https://chdtracelink.azurewebsites.net/', // production api US
         //apiRoot:'https://chdtracelink-hk.azurewebsites.net/', // production api HK
         //apiRoot:'https://chdtracelink-staging.azurewebsites.net/', // staging api
+        //apiRoot:'https://chdtracelinkdemo.azurewebsites.net/', // demo api
 
         appInsightsKey: 'b5491d97-cd4e-4bf2-a61c-a8abb12ef8e8', // us key
         //appInsightsKey: '6791db7f-193c-4370-8c23-763a233b35be', // hk key
 
         apiUser: 'apiuser',
-        apiPassword: 'I^Rf23i!VoV6',
+        //apiPassword: 'I^Rf23i!VoV6',
+        apiPassword: '9F*U&fvp7b', // demo
         secretCode: 'T0pS3cr3t',
         provider: 'CHD',
         dateFormat: 'yyyy-mm-dd'
--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ define([], function () {
     //    return;
     //}
 
-    var version = "1.4.3";
+    var version = "1.4.4";
     version = "_v=" + (version?version:(new Date()).getTime());
 
     var libs = {
