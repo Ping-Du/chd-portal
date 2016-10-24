0f77effa52783d2c4bbe96c3f4285ce2ad9a43e8
Ping Du <pingpng32@gmail.com>
2016-10-24 23:16:30 +0800

    change colors

--- scripts/config.js
+++ scripts/config.js
@@ -2,9 +2,9 @@ define([], function () {
     'use strict';
 
     return {
-        webRoot:'https://www.chdestination.com/', // production web US
+        //webRoot:'https://www.chdestination.com/', // production web US
         //webRoot:'https://chdtraceweb-hk.azurewebsites.net/', // production web HK
-        //webRoot: 'http://localhost:63342/chd-portal/', // local host
+        webRoot: 'http://localhost:63342/chd-portal/', // local host
         //webRoot: 'https://chdtracewebdemo.azurewebsites.net/', // demo site
 
         // -- apiRoot:'https://link.chdestination.com/', // production api US, not work
@@ -17,8 +17,8 @@ define([], function () {
         //appInsightsKey: '6791db7f-193c-4370-8c23-763a233b35be', // hk key
 
         apiUser: 'apiuser',
-        //apiPassword: 'I^Rf23i!VoV6',
-        apiPassword: '9F*U&fvp7b', // demo
+        apiPassword: 'I^Rf23i!VoV6',
+        //apiPassword: '9F*U&fvp7b', // demo
         secretCode: 'T0pS3cr3t',
         provider: 'CHD',
         dateFormat: 'yyyy-mm-dd'
